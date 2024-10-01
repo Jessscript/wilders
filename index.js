4a80eb90121764a81907d6b3682d3930ea3165ad
Jessy <jessy.pinchon@wildcodeschool.com>
2024-10-01 14:42:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Michel"));
+console.log(chalk.red("Géraldine"));
+console.log(chalk.bgYellow("Paul"));
+
+const name = "Agnès";
+console.log(chalk.underline(`Hello ${name}`));
